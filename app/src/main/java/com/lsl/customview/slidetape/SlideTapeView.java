f513c103d9948106d3faf8038a972914d90e0b70
lanshilei <lanshilei@weibangong.com>
2018-04-27 10:17:36 +0800

    代码结构优化

--- app/src/main/java/com/lsl/customview/slidetape/SlideTapeView.java
+++ app/src/main/java/com/lsl/customview/slidetape/SlideTapeView.java
@@ -15,8 +15,6 @@ import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.Scroller;
 
-import java.math.BigDecimal;
-
 /**
  * Created by lsl on 2018/04/17
  */
@@ -34,12 +32,19 @@ public class SlideTapeView extends View{
 
     private float mWidth;
     private float mHeight;
+    /**
+     * mScaleNum * mScaleSpace = mWidth
+     * 这两个属性赋值一个即可
+     */
     private int mScaleNum = 30;     //刻度线个数
     private float mScaleSpace;      //刻度线间距
-    private float mScaleGranularity = 0.1f; //刻度粒度
-    private int mInitScale = 0;     //初始刻度值
-    private int mMinScale = 0;      //最小刻度值
-    private int mMaxScale = 100;    //最大刻度值
+    private int mInitScale = 0;     //初始刻度
+    private int mMinScale = -100;   //最小刻度
+    private int mMaxScale = 100;    //最大刻度
+    private float mScaleGranularity = 1f;    //大刻度粒度
+    private float mFinerGranularity = 0.1f;   //小刻度粒度
+    private int mScaleCount = (int) (mScaleGranularity / mFinerGranularity);    //两条大刻度线之间的刻度数
+    private String mUnit = "kg";       //单位
 
     private float mLastTouchX;  //上一次的触摸点
 
@@ -99,14 +104,15 @@ public class SlideTapeView extends View{
                 if(parent != null) {
                     parent.requestDisallowInterceptTouchEvent(true);
                 }
+                //停止上一次还未完成的滑动
                 if(!mScroller.isFinished()) {
                     mScroller.abortAnimation();
                 }
                 mLastTouchX = event.getX();
                 break;
             case MotionEvent.ACTION_MOVE:
-                float minX = - (mInitScale - mMinScale) * mScaleSpace / mScaleGranularity;   //左边界坐标
-                float maxX = (mMaxScale - mInitScale) * mScaleSpace / mScaleGranularity;   //右边界坐标
+                float minX = - (mInitScale - mMinScale) * mScaleSpace;   //左边界坐标
+                float maxX = (mMaxScale - mInitScale) * mScaleSpace;   //右边界坐标
                 //右划未超过左边界，左划未超过右边界
                 if((event.getX() > mLastTouchX && getScrollX() > minX) ||
                         (event.getX() < mLastTouchX && getScrollX() < maxX)) {
@@ -156,8 +162,10 @@ public class SlideTapeView extends View{
         float mid = (mScaleSpace * left + mScaleSpace * right) / 2;
         int transX;
         if(translocation < mid) {
+            //离左边近
             transX = (int) (mScaleSpace * left - translocation);
         } else {
+            //离右边近
             transX = (int) (mScaleSpace * right - translocation);
         }
         mScroller.startScroll(getScrollX(), 0, transX, 0, 500);
@@ -165,8 +173,8 @@ public class SlideTapeView extends View{
     }
 
     private void fling(int x) {
-        float minX = - (mInitScale - mMinScale) * mScaleSpace / mScaleGranularity;   //左边界坐标
-        float maxX = (mMaxScale - mInitScale) * mScaleSpace / mScaleGranularity;   //右边界坐标
+        float minX = - (mInitScale - mMinScale) * mScaleSpace;   //左边界坐标
+        float maxX = (mMaxScale - mInitScale) * mScaleSpace;   //右边界坐标
         mScroller.fling(getScrollX(), 0, x, 0, (int) minX, (int) maxX, 0, 0);
         invalidate();
     }
@@ -190,8 +198,8 @@ public class SlideTapeView extends View{
      */
     private void drawHorizontalLine(Canvas canvas) {
         float translocation = getScrollX();
-        float minX = mWidth / 2 - (mInitScale - mMinScale) * mScaleSpace / mScaleGranularity;   //左边界坐标
-        float maxX = mWidth / 2 + (mMaxScale - mInitScale) * mScaleSpace / mScaleGranularity;   //右边界坐标
+        float minX = mWidth / 2 - (mInitScale - mMinScale) * mScaleSpace;   //左边界坐标
+        float maxX = mWidth / 2 + (mMaxScale - mInitScale) * mScaleSpace;   //右边界坐标
         canvas.drawLine(Math.max(translocation, minX), 0, Math.min(mWidth + translocation, maxX), 0, mScalePaint);
     }
 
@@ -199,18 +207,22 @@ public class SlideTapeView extends View{
      * 绘制刻度线以及刻度值
      */
     private void drawScaleLine(Canvas canvas) {
-        float translocation = getScrollX();
-        float startX = mInitScale - mScaleGranularity * mScaleNum / 2;     //第一条刻度线的值
-        for(float i = upNumber(translocation, mScaleSpace); i < mWidth + upNumber(translocation, mScaleSpace); i += mScaleNum) {
-            float scaleIndex = i / mScaleNum;
-            float scaleX = scaleIndex * mScaleSpace;      //当前刻度线的横坐标
-            float scaleValue = startX + scaleIndex * mScaleGranularity;   //当前刻度线的值
-            if(scaleValue >= mMinScale && scaleValue <= mMaxScale) {
-                if (scaleValue % 1 == 0) {
+        //只绘制屏幕内的刻度
+        for(int i = 0; i <= mScaleNum; i++) {
+            int index = i + getScaleTransNumber();
+
+            int scaleIndex = getIndexOfScale(index);          //当前刻度线的index
+            float scaleX = getXCoordinateOfScale(index);      //当前刻度线的横坐标
+
+            if(scaleIndex >= mMinScale && scaleIndex <= mMaxScale) {
+                if (scaleIndex % mScaleCount == 0) {
+                    //长刻度线
+                    float reciprocal = 1 / mFinerGranularity;
                     mScalePaint.setStrokeWidth(6);
                     canvas.drawLine(scaleX, 0, scaleX, 150, mScalePaint);
-                    canvas.drawText((int) scaleValue + "", scaleX, 220, mTextPaint);
+                    canvas.drawText(scaleIndex / reciprocal + "", scaleX, 220, mTextPaint);
                 } else {
+                    //短刻度线
                     mScalePaint.setStrokeWidth(4);
                     canvas.drawLine(scaleX, 0, scaleX, 75, mScalePaint);
                 }
@@ -227,14 +239,14 @@ public class SlideTapeView extends View{
         canvas.drawLine(mWidth / 2 + translocation, 0, mWidth / 2 + translocation, 170, mBaseScalePaint);
         //选中文字
         Rect rect = new Rect();
-        float reciprocal = 1 / mScaleGranularity;
-        String scaleText = mInitScale + ((int) (translocation / mScaleSpace)) / reciprocal + "";
+        float reciprocal = 1 / mFinerGranularity;
+        String scaleText = (mInitScale + (int) (translocation / mScaleSpace)) / reciprocal + "";
         mBaseScalePaint.setTextSize(100);
         mBaseScalePaint.getTextBounds(scaleText, 0, scaleText.length(), rect);
         canvas.drawText(scaleText + "", mWidth / 2 + translocation, -80, mBaseScalePaint);
         //单位
         mBaseScalePaint.setTextSize(50);
-        canvas.drawText("kg", mWidth / 2 + rect.right + translocation, rect.top - 80, mBaseScalePaint);
+        canvas.drawText(mUnit, mWidth / 2 + rect.right + translocation, rect.top - 80, mBaseScalePaint);
     }
 
     /**
@@ -262,20 +274,47 @@ public class SlideTapeView extends View{
     }
 
     /**
-     * 向上取整
-     * 根据x的范围返回值：
-     * (-72,-36] -> -mScaleNum
-     * (-36,0]   -> 0
-     * (0,36]    -> mScaleNum
-     * (36,72]   -> 2*mScaleNum
+     * 获取基准线左边的刻度线个数
      */
-    private float upNumber(float x, float y) {
-        if(x % y == 0) {
-            return (x / y) * mScaleNum;
-        } else if(x > 0){
-            return (int) (x / y + 1) * mScaleNum;
+    private int getScaleNumLeft() {
+        if(mScaleNum % 2 == 0) {
+            return mScaleNum / 2;
         } else {
-            return (int) (x / y) * mScaleNum;
+            return (mScaleNum - 1) / 2;
+        }
+    }
+
+    /**
+     * 返回当前刻度线的index
+     * 以刻度0位原点，向左为负，向右为正
+     * @param i 屏幕中的第几条刻度线
+     */
+    private int getIndexOfScale(int i) {
+        return mInitScale - getScaleNumLeft() + i;
+    }
+
+    /**
+     * 返回当前刻度线的横坐标
+     * @param i 屏幕中的第几条刻度线
+     */
+    private float getXCoordinateOfScale(int i) {
+        //第一条刻度线的横坐标，刻度线个数为偶数从0开始，为奇数从一半scaleSpace开始
+        float startX = mScaleNum % 2 * mScaleSpace / 2;
+        return startX + i * mScaleSpace;
+    }
+
+    /**
+     * 将横向的坐标位移转换为刻度的位移
+     * @return  当前已经移动了几条刻度线，向右为正，向左为负
+     */
+    private int getScaleTransNumber() {
+        float x = getScrollX();     //横向的坐标位移
+        if(x % mScaleSpace == 0) {
+            return (int) (x / mScaleSpace);
+        } else if(x > 0){
+            return (int) (x / mScaleSpace + 1);
+        } else {
+            return (int) (x / mScaleSpace);
         }
     }
 
@@ -283,6 +322,7 @@ public class SlideTapeView extends View{
     public void computeScroll() {
         if(mScroller.computeScrollOffset()) {
             scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            //滑动结束后停在最近的刻度线上
             if(!mScroller.computeScrollOffset()) {
                 scrollToScaleLine();
             }
